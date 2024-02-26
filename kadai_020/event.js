6800cc8c56b4895574582e51e65846859bb8a6dc
minami-u11 <156574901+minami-u11@users.noreply.github.com>
2024-02-26 16:33:45 +0900

    JS 20章課題

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+//btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+//textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+//HTML要素がクリックされた時にイベント処理を実行する
+btn.addEventListener('click', () => {
+
+  //テキストを取得する
+    text.textContent = 'ボタンをクリックしました';
+
+  });
